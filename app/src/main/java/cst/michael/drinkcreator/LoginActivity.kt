d6f94f70265f9591b5fd1616997f64c85fa81bdf
Michael Blake <michael.blake@oit.edu>
2018-05-23 13:47:59 -0700

    started implementation of Firebase

---
+++ app/src/main/java/cst/michael/drinkcreator/LoginActivity.kt
@@ -0,0 +1,59 @@
+package cst.michael.drinkcreator
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.firebase.ui.auth.AuthUI
+import java.util.*
+import java.util.Arrays.asList
+import android.R.attr.data
+import com.firebase.ui.auth.IdpResponse
+
+class LoginActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        login()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    fun login() {
+        val providers = Arrays.asList(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build())
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
+    }
+}
