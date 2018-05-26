700200f2c99fd34b00c91ad0fa5fdaa89e7d166b
michael <michael.blake@oit.edu>
2018-05-25 23:43:54 -0700

    code cleanup

--- app/src/main/java/cst/michael/drinkcreator/activities/CreateDrinkActivity.kt
+++ app/src/main/java/cst/michael/drinkcreator/activities/CreateDrinkActivity.kt
@@ -1,24 +1,19 @@
 package cst.michael.drinkcreator.activities
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.widget.CheckBox
 import android.widget.EditText
-import android.widget.RadioButton
 import android.widget.Toast
 import cst.michael.drinkcreator.R
 import cst.michael.drinkcreator.data.firebase.FirebaseDBHelper
 import kotlinx.android.synthetic.main.activity_create_drink.*
 import android.widget.ArrayAdapter
-import cst.michael.drinkcreator.R.id.baseDrinkSpinner
-
 
 class CreateDrinkActivity : AppCompatActivity() {
     private var drinkName = ""
     private val drinkFlavors = mutableListOf<String>()
-    private var baseDrink = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,31 +21,9 @@ class CreateDrinkActivity : AppCompatActivity() {
 
         title = "Create a New Drink"
 
-        val flavorArray = resources.getStringArray(R.array.flavor_options_array)
+        setupBaseDrinkMenu()
 
-        saveDrink.setOnClickListener {
-            getDrinkName()
-        }
-
-        val adapter = ArrayAdapter.createFromResource(this,
-                R.array.base_drink_options_array, android.R.layout.simple_spinner_item)
-        
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        baseDrinkSpinner.adapter = adapter
-
-        for (f in flavorArray) {
-            val checkbox = CheckBox(this)
-            checkbox.text = f
-            checkbox.setOnClickListener {
-                if (checkbox.isChecked) {
-                    drinkFlavors.add(checkbox.text.toString())
-                } else {
-                    drinkFlavors.remove(checkbox.text.toString())
-                }
-            }
-            //place flavor list right above the button in the layout.
-            drinkCreatorLayout.addView(checkbox)
-        }
+        setupListeners()
     }
 
     /*override fun onSaveInstanceState(outState: Bundle?) {
@@ -95,9 +68,38 @@ class CreateDrinkActivity : AppCompatActivity() {
     }
 
     private fun addDrink() {
-        //dbHelper.insertDrink(dbHelper.writableDatabase, drinkName, baseDrink, drinkFlavors.toString())
         val helper = FirebaseDBHelper()
         val baseDrink = baseDrinkSpinner.selectedItem.toString()
         helper.addDrink(drinkName, drinkFlavors, baseDrink)
+    }
+
+    private fun setupListeners() {
+        val flavorArray = resources.getStringArray(R.array.flavor_options_array)
+
+        for (f in flavorArray) {
+            val checkbox = CheckBox(this)
+            checkbox.text = f
+            checkbox.setOnClickListener {
+                if (checkbox.isChecked) {
+                    drinkFlavors.add(checkbox.text.toString())
+                } else {
+                    drinkFlavors.remove(checkbox.text.toString())
+                }
+            }
+            //place flavor list right above the button in the layout.
+            drinkCreatorLayout.addView(checkbox)
+        }
+
+        saveDrink.setOnClickListener {
+            getDrinkName()
+        }
+    }
+
+    private fun setupBaseDrinkMenu() {
+        val adapter = ArrayAdapter.createFromResource(this,
+                R.array.base_drink_options_array, android.R.layout.simple_spinner_item)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        baseDrinkSpinner.adapter = adapter
     }
 }
